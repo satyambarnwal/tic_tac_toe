599ec37fc378d936791da63a0734a5152cbc395d
Satyam Barnwal <satyambarnwal2002@gmail.com>
2024-12-23 13:11:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let newGameBtn = document.querySelector("#new-btn");
+let drawContainer = document.querySelector(".draw-container");
+let draw = document.querySelector("#draw");
+let drawNewBtn = document.querySelector("#draw-new-btn");
+
+let turnO = true; //playerO
+let count = 0;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+  drawContainer.classList.add("hidden");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+
+    count++;
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showDraw = () => {
+  draw.innerText = "Game is draw";
+  drawContainer.classList.remove("hidden");
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return;
+      }
+    }
+  }
+  if (count === 9) {
+    showDraw();
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+drawNewBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
